ee328bc252ae897c94e972769ecb31f5c9d161f9
Wei Zheng <zhengwei8618@gmail.com>
2024-01-12 15:06:08 +0800

    simple demo 2.3 Pipeline

--- wekascraper/pipelines.py
+++ wekascraper/pipelines.py
@@ -6,8 +6,43 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
 
 
 class WekascraperPipeline:
     def process_item(self, item, spider):
         return item
+
+
+class PriceToUSDPipeline:
+    gbpToUsdRate = 1.3
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        if adapter.get('price'):
+
+            float_price = float(adapter['price'])
+            adapter['price'] = float_price * self.gbpToUsdRate
+
+            return item
+        else:
+            raise DropItem(f"Missing price in {item}")
+
+
+class DuplicatesPipeline:
+
+    def __init__(self):
+        self.names_seen = set()
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        if adapter['name'] in self.names_seen:
+            raise DropItem(f"Duplicate item found {item}")
+        else:
+            self.names_seen.add(adapter['name'])
+            return item
+
+
+
